bf5a3fea38d7fcaf35fb26f1b6fb2ce2205a85d4
monthonkachana <maxmonthonkachana2@gmail.com>
2023-12-13 16:39:46 +0700

    test

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ var corsOptions = {
 app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
+// parse requests of content-type - application/json
+
 app.use(bodyParser.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
